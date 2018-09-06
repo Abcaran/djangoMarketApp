d30fa9c00899b6a5ce8258843377a96f8a0f51d9
Samir <samirsaadi@gmail.com>
2018-09-06 11:43:46 -0300

    Django Web Market app

---
+++ market/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Product(models.Model):
+	name = models.CharField (max_length=50)
+
+	def __str__(self):
+		return self.name
+
+class Purchase(models.Model):
+	product = models.ForeignKey (Product, on_delete=models.CASCADE)
+	amount = models.PositiveIntegerField()
+	total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+	def __str__(self):
+		message = 'O produto comprado foi '+self.product.name+', com '+str(self.amount)+' unidade(s), tendo o valor total de '+str(self.total_price)+'.'
+		return message
+
+
+
+
