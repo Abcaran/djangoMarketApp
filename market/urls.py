0235a964feca922e0705b5c89976cf88c41dc572
Samir <samirsaadi@gmail.com>
2018-09-07 23:42:46 -0300

    New: Web system fully functional

--- market/urls.py
+++ market/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('purchase-list/', views.purchase_list, name='purchase_list'),
+    path('', views.purchase_list, name='purchase_list'),
     path('product/new', views.product_new, name='product_new'),
     path('purchase/new', views.purchase_new, name='purchase_new'),
 ]
--- market/views.py
+++ market/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Purchase
 from .forms import ProductForm,PurchaseForm
+from django.db.models import Q
 
 
 # Create your views here.
@@ -8,16 +9,21 @@ def purchase_list(request):
 
 	all_purchases = Purchase.objects.all()
 
+	query = request.GET.get("nome_produto")
+	if query:
+		all_purchases = all_purchases.filter(Q(product__name__icontains=query))
+
 	return render(request, 'purchase_list.html', {'all_purchases': all_purchases})
 
 def product_new(request):
+
 	if request.method == "POST":
 		form = ProductForm(request.POST)
 		if form.is_valid():
 			product = form.save()
+			return redirect(purchase_new)
 	else:
 		form = ProductForm()
-
 	return render(request, 'product_new.html', {'form': form})
 
 
@@ -28,7 +34,7 @@ def purchase_new(request):
 			purchase = form.save(commit=False)
 			purchase.avrg_price = float(form.cleaned_data['total_price'])/float(form.cleaned_data['amount'])
 			purchase.save()
+			return redirect(purchase_list)
 	else:
 		form = PurchaseForm()
-
 	return render(request, 'purchase_new.html', {'form':form})
